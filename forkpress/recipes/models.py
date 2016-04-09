1f8fc6f4fb1d291365317d7ae79c8da09a81118e
ktarrant <kmtarrant13@gmail.com>
2016-04-09 00:39:41 -0400

    fix url and add model draft

--- forkpress/recipes/models.py
+++ forkpress/recipes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    unitValue = models.FloatField(default=0.0)
+    unitName = models.CharField(max_length=200)
+    calories = models.IntegerField(default=0)
+    protein = models.DecimalField(default=0.0, max_digits=7, decimal_places=2)
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    step = models.ForeignKey('Step', on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    portion = models.FloatField(default=0.0)
+
+class Step(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    number = models.IntegerField(default=1)
+    instruction = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient)
+
+class Recipe(models.Model):
+    pub_date = models.DateTimeField('date published')
+    ingredients = models.ManyToManyField(Ingredient)
+    steps = modles.ManyToManyField(Step)
